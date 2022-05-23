90e1e60342cc32edebfa9bfa70a5eb3bc73f26ce
Allen Ding <aqding@mit.edu>
2022-05-23 02:33:02 -0400

    adding classes spring 2022

--- client/src/components/modules/Classes.js
+++ client/src/components/modules/Classes.js
@@ -10,10 +10,13 @@ const Classes = () => {
         <div className="class">6.864 — Advanced Natural Language Processing (G) </div>
         <div className="class">6.865 — Advanced Computational Photography (G) </div>
         <div className="class">6.046 — Design and Analysis of Algorithms</div>
-        <div className="class">6.004 — Computation Structures</div>
+        <div className="class">6.031 — Elements of Software Construction </div>
+        <div className="class">6.033 — Computer Systems Engineering </div>
       </div>
       <div className="classesColumn">
         <div className="class">6.148 — Web Lab</div>
+        <div className="class">6.08 — Intro to EECS via Interconnected Embedded Systems</div>
+        <div className="class">6.004 — Computation Structures</div>
         <div className="class">6.036 — Introduction to Machine Learning</div>
         <div className="class">6.006 — Introduction to Algorithms</div>
         <div className="class">6.009 — Fundamentals of Programming</div>
